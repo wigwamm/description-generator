b717c462327a8c22c69b76c0588c3378949c49b0
Guy Routledge <guy@bigfish.co.uk>
2013-11-27 07:49:28 +0000

    Add Route, Controller and Model for Questions
    * Stub out Questions model with questions for either Sales or Lettings
    * Provide the question label, array of answers and a slug which can be
    used as a nice-name or class hook for styling.
    * Consider adding in a property that specifies the input[type] for
    building the actual form fields.
    * This model only describes the questions and how they should be output
    rather than the datafile that will be generated when answering them -
    this will need to be built in a way that is compatible with Blabber app

---
+++ app/js/questions/questions-controller.js
@@ -0,0 +1,6 @@
+angular.module('app').controller('QuestionsController', function($scope, $location, QuestionsModel) {
+
+  $scope.title = 'Proppycock';
+  $scope.intro = 'Property descriptions are a load of poppycock. Poke fun and create a proppycock description for your property and share with the world.';
+	$scope.questions = QuestionsModel;
+});
---
+++ app/js/questions/questions-model.js
@@ -0,0 +1,154 @@
+angular.module('app').factory('QuestionsModel', function() {
+	var questions = {};
+	questions.sales = [
+		{
+			'question' : 'What type of property?',
+			'answers'  : [ 'Detached', 'Semi-detached', 'Terraced', 'Flat - Period conversion', 'Flat - block'],
+			'slug'     : 'propertyType'
+		},
+		{
+			'question' : 'How many bedrooms?',
+			'answers'  : ['Studio', '1', '2', '3', '4', '5', '6', '7'],
+			'slug'     : 'numberOfBedrooms'
+		},
+		{
+			'question' : 'What is the postcode of the property?',
+			'answers'  : '',
+			'slug'     : 'postCode'
+		},
+		{
+			'question' : 'What\'s the asking price?',
+			'answers'  : '',
+			'slug'     : 'price'
+		},
+		{
+			'question' : 'Which floor is the property on?',
+			'answers'  : ['B', 'G', [1..40]],
+			'slug'     : ''
+		},
+		{
+			'question' : 'Is there a lift?',
+			'answers'  : ['Yes', 'No'],
+			'slug'     : 'lift'
+		},
+		{
+			'question' : 'Does the property have a rear garden',
+			'answers'  : ['None', 'Small', 'Large', 'Patio', 'Balcony'],
+			'slug'     : 'gardenRear'
+		},
+		{
+			'question' : 'Is there parking?',
+			'answers'  : ['None', 'On-street', 'Drive', 'Single Garage', 'Double Garage'],
+			'slug'     : 'parking'
+		},
+		{
+			'question' : 'When was the property last decorated?',
+			'answers'  : ['This year', '%lastYear', '%twoYearsAgo', '%threeYearsAgo or earlier'],
+			'slug'     : 'decoratedYear'
+		},
+		{
+			'question' : 'Is the property Freehold or Leasehold?',
+			'answers'  : ['Freehold', 'Leasehold'],
+			'slug'     : 'tenure'
+		},
+		{
+			'question' : 'How many years left on the lease',
+			'answers'  : '',
+			'slug'     : 'leaseRemaining'
+		},
+		{
+			'question' : 'Is there a service charge or ground rent?',
+			'answers'  : '',
+			'slug'     : 'serviceCharge'
+		},
+		{
+			'question' : 'Why are you selling?',
+			'answers'  : ['Moving to a new area', 'Down-sizing', 'Up-sizing'],
+			'slug'     : 'reasonForSelling'
+		}
+	];
+	questions.lettings = [
+		{
+			'question' : 'What type of property?',
+			'answers'  : [ 'Detached', 'Semi-detached', 'Terraced', 'Flat - Period conversion', 'Flat - block', 'Room to let'],
+			'slug'     : 'propertyType'
+		},
+		{
+			'question' : 'How many bedrooms?',
+			'answers'  : ['Studio', '1', '2', '3', '4', '5', '6', '7'],
+			'slug'     : 'numberOfBedrooms'
+		},
+		{
+			'question' : 'What is the postcode of the property?',
+			'answers'  : '',
+			'slug'     : 'postCode'
+		},
+		{
+			'question' : 'What\'s the rental price per calendar month?',
+			'answers'  : '',
+			'slug'     : 'price'
+		},
+		{
+			'question' : 'Which floor is the property on?',
+			'answers'  : ['B', 'G', [1..40]],
+			'slug'     : ''
+		},
+		{
+			'question' : 'Is there a lift?',
+			'answers'  : ['Yes', 'No'],
+			'slug'     : 'lift'
+		},
+		{
+			'question' : 'Does the property have a rear garden',
+			'answers'  : ['None', 'Small', 'Large', 'Patio', 'Balcony'],
+			'slug'     : 'gardenRear'
+		},
+		{
+			'question' : 'Is there parking?',
+			'answers'  : ['None', 'On-street', 'Drive', 'Single Garage', 'Double Garage'],
+			'slug'     : 'parking'
+		},
+		{
+			'question' : 'When was the property last decorated?',
+			'answers'  : ['This year', '%lastYear', '%twoYearsAgo', '%threeYearsAgo or earlier'],
+			'slug'     : 'decoratedYear'
+		},
+		{
+			'question' : 'Move in date?',
+			'answers'  : '',
+			'slug'     : 'moveInDate'
+		},
+		{
+			'question' : 'Is the property furnished?',
+			'answers'  : ['Furnished', 'Unfurnished'],
+			'slug'     : 'furnished'
+		},
+		{
+			'question' : 'Are pets allowed?',
+			'answers'  : ['Pets allowed', 'Caged pets only', 'No pets allowed'],
+			'slug'     : 'pets'
+		},
+		{
+			'question' : 'How much deposit is required?',
+			'answers'  : ['1 month', '6 weeks', '2 months'],
+			'slug'     : 'deposit'
+		},
+		{
+			'question' : 'What are the agent fees?',
+			'answers'  : '',
+			'slug'     : 'agentFees'
+		},
+		{
+			'question' : 'Who is the property managed by?',
+			'answers'  : ['Agent', 'Landlord'],
+			'slug'     : 'managedBy'
+		},
+		{
+			'question' : 'What bills are included in the rental price?',
+			'answers'  : ['Water', 'Council Tax', 'Energy', 'Phone', 'Broadband', 'TV License'],
+			'slug'     : 'billsIncluded'
+		}
+	];
+	return questions;
+
+});
