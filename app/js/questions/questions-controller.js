983f2161a20ea699a8c0de66522b6233ad1d8e93
Guy Routledge <guy@bigfish.co.uk>
2013-11-30 12:09:09 +0000

    Add ability to set the active question
    * Use ngClass and ngClick to set an `is-active` class when a question
    is clicked.
    * Add temporary <style> block for visually indicating active question

--- app/js/questions/questions-controller.js
+++ app/js/questions/questions-controller.js
@@ -3,4 +3,11 @@ angular.module('app').controller('QuestionsController', function($scope, $locati
 	$scope.model = {};
 	$scope.model.questions = QuestionsModel.all;
 
+	$scope.makeActive = function(item) {
+		$scope.active = item;
+	};
+	$scope.activeClass = function(item) {
+		return item === $scope.active ? 'is-active' : undefined;
+	};
+
 });
