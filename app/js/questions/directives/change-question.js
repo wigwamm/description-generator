9db16f09971c5579bde336debb5992ea857a55c3
Guy Routledge <guy@bigfish.co.uk>
2013-12-04 17:02:03 +0000

    Fix bug when changing to next question from sub-question
    When on a sub-question, the incorrect next question was being selected.
    This method of fixing things works but perhaps is a little fragile as
    if a question had multiple nested sub-questions, many levels deep, the
    whole thing would need re-writing.

--- app/js/questions/directives/change-question.js
+++ app/js/questions/directives/change-question.js
@@ -3,16 +3,21 @@ angular.module('app').directive('changeQuestion', function() {
 		link: function(scope, element, attrs){
 
 			element.bind('click', function(){
+
 				var nextQuestion, nextQuestionNumber;
-				var lastQuestion = scope.$parent.$index === (scope.model.questions.length - 1);
-				var noFurtherSubQuestion = scope.subQuestion || !scope.question.hasSub || scope.$index > scope.question.subQuestions.length - 1;
+				var lastQuestion  = scope.$parent.$index === (scope.model.questions.length - 1);
+				var isSubQuestion = scope.subQuestion;
+				var noSubQuestion = !scope.question.hasSub || (scope.$index > scope.question.subQuestions.length - 1);
 
 				if ( lastQuestion ) {
 					scope.postAnswers();
 					return;
 				}
 
-				if ( noFurtherSubQuestion ) {
+				if ( isSubQuestion ) {
+					nextQuestionNumber = scope.$parent.$parent.$index + 1;
+					nextQuestion       = scope.model.questions[nextQuestionNumber];
+				} else if ( noSubQuestion ) {
 					nextQuestionNumber = scope.$parent.$index + 1;
 					nextQuestion       = scope.model.questions[nextQuestionNumber];
 				} else {
